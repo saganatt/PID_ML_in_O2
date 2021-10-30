ff587523b816327743de1d624ba07a9402e7af5c
saganatt <8majak8@gmail.com>
2021-10-30 21:23:46 +0200

    Refreshed ROOT data with NSigma information

--- python/run.sh
+++ python/run.sh
@@ -2,27 +2,28 @@
 
 IN_DIR=/home/maja/store_part/store/PID_data
 OUT_DIR=/home/maja/CERN_part/CERN/PID_ML_in_O2/python/data/root
+
 train=208
 dataset=LHC18g4
 run=285064
-
 for number in {001..010}; do
   # Real tracks
-  o2-analysis-pp-pid-ml-producer --aod-file ${IN_DIR}/${dataset}/${run}/AO2D_${number}.root -b \
-    --aod-writer-keep AOD/PIDTRACKSREAL/0:::${OUT_DIR}/train_${train}_dataset_${dataset}_run_${run}_${number}_pidtracksreal_globaltracks |
-    o2-analysis-trackextension -b | o2-analysis-trackselection -b | o2-analysis-pid-tof -b
+  o2-analysis-trackextension -b | o2-analysis-trackselection -b | o2-analysis-pid-tof -b | o2-analysis-pid-tof-full -b | o2-analysis-pid-tpc-full -b |
+    o2-analysis-pp-pid-ml-producer --aod-file ${IN_DIR}/${dataset}/${run}/AO2D_${number}.root -b \
+    --aod-writer-keep AOD/PIDTRACKSREAL/0:::${OUT_DIR}/train_${train}_dataset_${dataset}_run_${run}_${number}_pidtracksreal_globaltracks
 
   # MC tracks
-  o2-analysis-pp-pid-ml-producer --aod-file ${IN_DIR}/${dataset}/${run}/AO2D_${number}.root -b --doMC \
-    --aod-writer-keep AOD/PIDTRACKSMC/0:::${OUT_DIR}/train_${train}_dataset_${dataset}_run_${run}_${number}_pidtracksmc_globaltracks |
-    o2-analysis-trackextension -b | o2-analysis-trackselection -b | o2-analysis-pid-tof -b
+  o2-analysis-trackextension -b | o2-analysis-trackselection -b | o2-analysis-pid-tof -b | o2-analysis-pid-tof-full -b | o2-analysis-pid-tpc-full -b |
+    o2-analysis-pp-pid-ml-producer --aod-file ${IN_DIR}/${dataset}/${run}/AO2D_${number}.root -b --doMC \
+    --aod-writer-keep AOD/PIDTRACKSMC/0:::${OUT_DIR}/train_${train}_dataset_${dataset}_run_${run}_${number}_pidtracksmc_globaltracks
 done
 
 train=204
 dataset=LHC18b
+run=285064
 for number in {001..010}; do
   # Real tracks
-  o2-analysis-pp-pid-ml-producer --aod-file ${IN_DIR}/${dataset}/${run}/AO2D_${number}.root -b \
-    --aod-writer-keep AOD/PIDTRACKSREAL/0:::${OUT_DIR}/train_${train}_dataset_${dataset}_run_${run}_${number}_pidtracksreal_globaltracks |
-    o2-analysis-trackextension -b | o2-analysis-trackselection -b | o2-analysis-pid-tof -b
+  o2-analysis-trackextension -b | o2-analysis-trackselection -b | o2-analysis-pid-tof -b | o2-analysis-pid-tof-full -b | o2-analysis-pid-tpc-full -b |
+    o2-analysis-pp-pid-ml-producer --aod-file ${IN_DIR}/${dataset}/${run}/AO2D_${number}.root -b \
+    --aod-writer-keep AOD/PIDTRACKSREAL/0:::${OUT_DIR}/train_${train}_dataset_${dataset}_run_${run}_${number}_pidtracksreal_globaltracks
 done
