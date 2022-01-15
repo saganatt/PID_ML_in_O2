c811aed237aa2ec008b98e1c4e5b020965dd3195
saganatt <8majak8@gmail.com>
2022-01-15 09:45:08 +0100

    Update run.sh with new exe name

--- python/run.sh
+++ python/run.sh
@@ -9,7 +9,7 @@ dataset=LHC18g4
 run=285064
 for number in {001..010}; do
   o2-analysis-timestamp -b | o2-analysis-trackextension -b | o2-analysis-trackselection -b | o2-analysis-pid-tof -b | o2-analysis-pid-tof-beta -b | o2-analysis-pid-tof-full -b | o2-analysis-pid-tpc-full -b |
-    o2-analysis-pp-pid-ml-producer --aod-file ${IN_DIR}/${dataset}/${run}/train_${train}/AO2D_${number}.root -b --doMC \
+    o2-analysis-pid-ml-producer --aod-file ${IN_DIR}/${dataset}/${run}/train_${train}/AO2D_${number}.root -b --doMC \
     --aod-writer-keep AOD/PIDTRACKSMC/0:::${OUT_DIR}/train_${train}_dataset_${dataset}_run_${run}_${number}_pidtracksmc_globaltracks
 done
 
@@ -19,6 +19,6 @@ dataset=LHC18b
 run=285064
 for number in {001..010}; do
   o2-analysis-timestamp -b | o2-analysis-trackextension -b | o2-analysis-trackselection -b | o2-analysis-pid-tof -b | o2-analysis-pid-tof-beta -b | o2-analysis-pid-tof-full -b | o2-analysis-pid-tpc-full -b |
-    o2-analysis-pp-pid-ml-producer --aod-file ${IN_DIR}/${dataset}/${run}/train_${train}/AO2D_${number}.root -b \
+    o2-analysis-pid-ml-producer --aod-file ${IN_DIR}/${dataset}/${run}/train_${train}/AO2D_${number}.root -b \
     --aod-writer-keep AOD/PIDTRACKSREAL/0:::${OUT_DIR}/train_${train}_dataset_${dataset}_run_${run}_${number}_pidtracksreal_globaltracks
 done
