47aebe06483b98bc6ff5b137a67e71156bd4d928
saganatt <8majak8@gmail.com>
2022-06-16 13:45:33 +0200

    Update run script

--- python/run.sh
+++ python/run.sh
@@ -1,24 +1,24 @@
 #!/bin/bash
 
 IN_DIR=/home/maja/store_part/store/PID_data
-OUT_DIR=/home/maja/CERN_part/CERN/PID_ML_in_O2/python/data/
+OUT_DIR=/home/maja/CERN_part/CERN/PIDML/PID_ML_in_O2/python/data
 
 # MC tracks
-train=208
+train=246
 dataset=LHC18g4
 run=285064
 for number in {001..010}; do
-  o2-analysis-timestamp -b | o2-analysis-trackextension -b | o2-analysis-trackselection -b | o2-analysis-pid-tof -b | o2-analysis-pid-tof-beta -b | o2-analysis-pid-tof-full -b | o2-analysis-pid-tpc-full -b |
+  o2-analysis-timestamp --isRun2MC -b | o2-analysis-centrality-table -b | o2-analysis-multiplicity-table -b | o2-analysis-trackextension -b | o2-analysis-trackselection -b | o2-analysis-pid-tof -b | o2-analysis-pid-tof-beta -b | o2-analysis-pid-tof-full -b | o2-analysis-pid-tpc-full -b |
     o2-analysis-pid-ml-producer --aod-file ${IN_DIR}/${dataset}/${run}/train_${train}/AO2D_${number}.root -b --doMC \
     --aod-writer-keep AOD/PIDTRACKSMC/0:::${OUT_DIR}/train_${train}_dataset_${dataset}_run_${run}_${number}_pidtracksmc_globaltracks
 done
 
 # Real tracks
-train=204
+train=267
 dataset=LHC18b
 run=285064
 for number in {001..010}; do
-  o2-analysis-timestamp -b | o2-analysis-trackextension -b | o2-analysis-trackselection -b | o2-analysis-pid-tof -b | o2-analysis-pid-tof-beta -b | o2-analysis-pid-tof-full -b | o2-analysis-pid-tpc-full -b |
+  o2-analysis-timestamp --isRun2MC -b | o2-analysis-centrality-table -b | o2-analysis-multiplicity-table -b | o2-analysis-trackextension -b | o2-analysis-trackselection -b | o2-analysis-pid-tof -b | o2-analysis-pid-tof-beta -b | o2-analysis-pid-tof-full -b | o2-analysis-pid-tpc-full -b |
     o2-analysis-pid-ml-producer --aod-file ${IN_DIR}/${dataset}/${run}/train_${train}/AO2D_${number}.root -b \
     --aod-writer-keep AOD/PIDTRACKSREAL/0:::${OUT_DIR}/train_${train}_dataset_${dataset}_run_${run}_${number}_pidtracksreal_globaltracks
 done
